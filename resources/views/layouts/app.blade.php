9c1498a78a4c8e96dd211c9e56611c0c8fbf67db
chafik <chafik.hnn@gmail.com>
2020-06-04 04:37:44 +0100

    forth deploy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Ecommerce') }}</title>
 
     <!--font awesome-->
     <link rel="stylesheet" href="{{ asset('dist/css/font-awesome5.11.2.min.css') }}">
